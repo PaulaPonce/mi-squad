bf164af5ed6230c24b0134271acea53a4d56e248
Paula Ponce <paula.poncez@usach.cl>
2017-05-04 23:29:45 -0300

    llenar arreglo

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,29 @@ function MiembroSquad(nombre, apellido, edad, hobbies){
 	this.hobbies = hobbies
 }
 
-var marcela = new MiembroSquad("Marcela", "Cabello", 31, ["Cantar", "Yoga", "Cocinar"]);
-var melissa = new MiembroSquad("Melissa", "Pacheco", 25, ["Dormir", "Comer", "Matilda"]);
-var paulina = new MiembroSquad("Paulina", "Aros", 28, ["Hacer libretas", "Acariciar gatos", "Manicure"]);
-var karla = new MiembroSquad("Karla", "Jeria", 35, ["Comer chocolates", "Viajar", "Ir al cine"]);
-var tanya = new MiembroSquad("Tanya", "Ramirez", 20, ["Gatos", "Viajar", "Tomar tecito a las 4"]);
-var amala = new MiembroSquad("Valentina Amala", "Kamala", 29, ["Bordar", "Tejer", "Dormir"]);
-var paula = new MiembroSquad("Paula", "Ponce", 28, ["Música", "Aves", "Heroes of the Storm"]);
-var mariela = new MiembroSquad("Mariela", "Cubillos", 36, ["Tejer", "Comer chocolate", "Cocinar"]);
+function mostrarMiembros(){
+	var miembros = []; //arreglo para guardar los miembros del squad
 
-var miembros = 
+	//instancia para cada miembro
+	var marcela = new MiembroSquad("Marcela", "Cabello", 31, ["Cantar", "Yoga", "Cocinar"]);
+	var melissa = new MiembroSquad("Melissa", "Pacheco", 25, ["Dormir", "Comer", "Matilda"]);
+	var paulina = new MiembroSquad("Paulina", "Aros", 28, ["Hacer libretas", "Acariciar gatos", "Manicure"]);
+	var karla = new MiembroSquad("Karla", "Jeria", 35, ["Comer chocolates", "Viajar", "Ir al cine"]);
+	var tanya = new MiembroSquad("Tanya", "Ramirez", 20, ["Gatos", "Viajar", "Tomar tecito a las 4"]);
+	var amala = new MiembroSquad("Valentina Amala", "Kamala", 29, ["Bordar", "Tejer", "Dormir"]);
+	var paula = new MiembroSquad("Paula", "Ponce", 28, ["Música", "Aves", "Heroes of the Storm"]);
+	var mariela = new MiembroSquad("Mariela", "Cubillos", 36, ["Tejer", "Comer chocolate", "Cocinar"]);
+
+	//llenar el arreglo miembros[]
+	miembros.push(marcela);
+	miembros.push(melissa);
+	miembros.push(paulina);
+	miembros.push(karla);
+	miembros.push(tanya);
+	miembros.push(amala);
+	miembros.push(paula);
+	miembros.push(mariela);
+}
+mostrarMiembros();
+
 document.getElementById('cont-7').innerHTML = paula.nombre;
-//hobbie1, hobbie2, hobbie3
