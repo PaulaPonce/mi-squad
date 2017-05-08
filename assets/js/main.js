73e28e4a8b892d4593d09ee81a65490da1bc1ba6
Paula Ponce <paula.poncez@usach.cl>
2017-05-08 00:09:50 -0300

    agregar fotos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 var miembros = []; //arreglo para guardar los miembros del squad
+var comentarios = []; //arreglo para guardar comentarios
 
-function MiembroSquad(nombre, apellido, edad, hobbies){
+function MiembroSquad(nombre, apellido, edad, hobbies, id){
 	this.nombre = nombre;
 	this.apellido = apellido;
 	this.edad = edad;
-	this.hobbies = hobbies
+	this.hobbies = hobbies;
+	this.id = id
 }
 
 function Comentario(id_miembro, comentario, likes){
@@ -43,33 +45,55 @@ function principal(){
 function llenarDiv(idDiv, arraySquad){ //idDiv-->id='contendor' //arraySquad-->miembros[]
 	var div = document.getElementById(idDiv); //<section id="contenedor"></section>
 	
-	var divAux; //sección: <div>
+	var divAux, divAux2; //sección: <div>
+	var imgAux; //imagen: <img>
 	var cajaAux; //área de texto: <textarea>
 	var botonAux; //botón: <button>
-	var botonLike; //botón para dar like a comentarios: <button>
+	var botonLike; //botón para dar like a comentarios: <input>
+	
 
 	arraySquad.forEach(function(elemento){
 		divAux = document.createElement('div'); //crea nodo <div>
+		div.appendChild(divAux); //enlaza <div> a <section>
+
+		imgAux = document.createElement('img'); //crea imagen <img>
+		imgAux.src = 'assets/img/' + elemento.apellido + '.jpg';
+		imgAux.setAttribute('width', '150');
+		imgAux.setAttribute('heigth', '150');
+		divAux.appendChild(imgAux); //enlaza <img> a <div>
+
+
+		divAux2 = document.createElement('div'); //crea nodo <div>
+		//contenido del <div>
+		divAux2.innerHTML += "<br><b>Nombre: </b>" + elemento.nombre + " " + elemento.apellido + "<br><b>Edad: </b>" + elemento.edad + "<br><b>Hobbies:</b><br>";
+		var listaHobbie = "<ul>" + elemento.hobbies.forEach(function(h){
+			return(divAux2.innerHTML += "<li>" + h + "</li>");}) + "</ul>";
+		//divAux2.setAttribute('id', 'cont-miembros'); //<div id='cont-miembros'>
+		divAux.appendChild(divAux2); //enlaza <div> a <section>
+
 		cajaAux = document.createElement('textarea'); //crea área de texto <textarea>
+		divAux.appendChild(cajaAux); //enlaza <textarea> a <div>
+		cajaAux.setAttribute('id', elemento.id); //<textarea id='caja-com'>
+
 		botonAux = document.createElement('button'); //crea botón <button>
+		divAux.appendChild(botonAux); //enlaza <button> a <div>
+		botonAux.setAttribute('onclick', 'anadirComentario()'); //función del botón
+		botonAux.innerHTML = "Añadir Comentario"; //contenido del botón		
+
+		var divTxt = document.createElement('div');
+		divTxt.setAttribute('id', elemento.id);
+		divAux2.appendChild(divTxt);
+
 		botonLike = document.createElement('input'); //crea input <input>, botón con imagen
 		botonLike.src = "assets/img/red-heart.png";
 		botonLike.type = "image";
-		
-		//contenido del <div>
-		divAux.innerHTML += "<br><b>Nombre: </b>" + elemento.nombre + " " + elemento.apellido + "<br><b>Edad: </b>" + elemento.edad + "<br><b>Hobbies:</b><br>";
-		var listaHobbie = "<ul>" + elemento.hobbies.forEach(function(h){
-			return(divAux.innerHTML += "<li>" + h + "</li>");}) + "</ul>";
-		
-		div.appendChild(divAux); //enlaza <div> a <section>
-		divAux.appendChild(cajaAux); //enlaza <textarea> a <div>
-		divAux.appendChild(botonAux); //enlaza <button> a <div>
-		//botonAux.setAttribute(name, value);
-		botonAux.innerHTML = "Añadir Comentario"; //contenido del botón
 		divAux.appendChild(botonLike); //enlaza <input> a <div>
-		botonLike.setAttribute("width", "20");
-		botonLike.setAttribute("heigth", "20");
-
+		botonLike.setAttribute('width', '20');
+		botonLike.setAttribute('heigth', '20');
 	});
 }
+
+
+
+
 principal();
