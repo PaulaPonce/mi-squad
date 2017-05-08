5110b4ac6f85d72c95de159dd31e08217ab3abc4
Paula Ponce <paula.poncez@usach.cl>
2017-05-08 01:04:35 -0300

    función anadirComentarios() para el botón

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,14 @@ function Comentario(id_miembro, comentario, likes){
 
 function principal(){
 	//instancia para cada miembro
-	var marcela = new MiembroSquad("Marcela", "Cabello", 31, ["Cantar", "Yoga", "Cocinar"]);
-	var melissa = new MiembroSquad("Melissa", "Pacheco", 25, ["Dormir", "Comer", "Matilda"]);
-	var paulina = new MiembroSquad("Paulina", "Aros", 28, ["Hacer libretas", "Acariciar gatos", "Manicure"]);
-	var karla = new MiembroSquad("Karla", "Jeria", 35, ["Comer chocolates", "Viajar", "Ir al cine"]);
-	var tanya = new MiembroSquad("Tanya", "Ramirez", 20, ["Gatos", "Viajar", "Tomar tecito a las 4"]);
-	var amala = new MiembroSquad("Valentina Amala", "Kamala", 29, ["Bordar", "Tejer", "Dormir"]);
-	var paula = new MiembroSquad("Paula", "Ponce", 28, ["Música", "Aves", "Heroes of the Storm"]);
-	var mariela = new MiembroSquad("Mariela", "Cubillos", 36, ["Tejer", "Comer chocolate", "Cocinar"]);
+	var marcela = new MiembroSquad("Marcela", "Cabello", 31, ["Cantar", "Yoga", "Cocinar"], 1);
+	var melissa = new MiembroSquad("Melissa", "Pacheco", 25, ["Dormir", "Comer", "Matilda"], 2);
+	var paulina = new MiembroSquad("Paulina", "Aros", 28, ["Hacer libretas", "Acariciar gatos", "Manicure"], 3);
+	var karla = new MiembroSquad("Karla", "Jeria", 35, ["Comer chocolates", "Viajar", "Ir al cine"], 4);
+	var tanya = new MiembroSquad("Tanya", "Ramirez", 20, ["Gatos", "Viajar", "Tomar tecito a las 4"], 5);
+	var amala = new MiembroSquad("Valentina Amala", "Kamala", 29, ["Bordar", "Tejer", "Dormir"], 6);
+	var paula = new MiembroSquad("Paula", "Ponce", 28, ["Música", "Aves", "Heroes of the Storm"], 7);
+	var mariela = new MiembroSquad("Mariela", "Cubillos", 36, ["Tejer", "Comer chocolate", "Cocinar"], 8);
 
 	//llenar el arreglo miembros[]
 	miembros.push(marcela);
@@ -68,20 +68,22 @@ function llenarDiv(idDiv, arraySquad){ //idDiv-->id='contendor' //arraySquad-->m
 		divAux2.innerHTML += "<br><b>Nombre: </b>" + elemento.nombre + " " + elemento.apellido + "<br><b>Edad: </b>" + elemento.edad + "<br><b>Hobbies:</b><br>";
 		var listaHobbie = "<ul>" + elemento.hobbies.forEach(function(h){
 			return(divAux2.innerHTML += "<li>" + h + "</li>");}) + "</ul>";
-		//divAux2.setAttribute('id', 'cont-miembros'); //<div id='cont-miembros'>
 		divAux.appendChild(divAux2); //enlaza <div> a <section>
 
 		cajaAux = document.createElement('textarea'); //crea área de texto <textarea>
-		divAux.appendChild(cajaAux); //enlaza <textarea> a <div>
-		cajaAux.setAttribute('id', elemento.id); //<textarea id='caja-com'>
+		divAux2.appendChild(cajaAux); //enlaza <textarea> a <div>
+		cajaAux.setAttribute('id', elemento.id); //<textarea id=elemento.id>
+		cajaAux.setAttribute('class', 'caja-com');
+		cajaAux.setAttribute('type', 'text');
 
 		botonAux = document.createElement('button'); //crea botón <button>
 		divAux.appendChild(botonAux); //enlaza <button> a <div>
-		botonAux.setAttribute('onclick', 'anadirComentario()'); //función del botón
+		botonAux.setAttribute('onclick', 'anadirComentario(this.cajaAux, this,' + elemento.id + ', this.divTxt)'); //función del botón
 		botonAux.innerHTML = "Añadir Comentario"; //contenido del botón		
 
-		var divTxt = document.createElement('div');
+		var divTxt = document.createElement('div'); //crea nodo <div> para comentarios
 		divTxt.setAttribute('id', elemento.id);
+		divTxt.setAttribute('class', 'comentarios');
 		divAux2.appendChild(divTxt);
 
 		botonLike = document.createElement('input'); //crea input <input>, botón con imagen
@@ -93,7 +95,22 @@ function llenarDiv(idDiv, arraySquad){ //idDiv-->id='contendor' //arraySquad-->m
 	});
 }
 
+function anadirComentario(cajaAux, botonAux, elId, divTxt){
+	cajaAux = document.getElementById(elId).value;
+	divTxt = document.getElementsByClassName('comentarios');
 
+	var filtro = miembros.filter(function(f){
+		return f.id == elId;
+	})
 
+	var com = document.createElement('div');
+	com.setAttribute('id', elId);
+	com.innerHTML = "~ " + document.getElementById(elId).value;
 
+	if(filtro[0].id == elId && cajaAux != ""){
+		comentarios.push(com);
+		divTxt[filtro[0].id - 1].appendChild(com);
+		document.getElementById(elId).value = "";
+	}
+}
 principal();
